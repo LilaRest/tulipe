1f2728751ac96a3182761c67eb3796eea857da1e
Lila Rest <mail@lila.rest>
2022-08-12 11:55:28 +0200

    Improve ContractInteractor component's code

--- src/components/_ContractInteractor.vue
+++ src/components/_ContractInteractor.vue
@@ -8,28 +8,42 @@ const props = defineProps({
     required: true
   }
 })
-const contract = dapp.contracts[props.contractName]
 
+const contract = dapp.contracts[props.contractName]
 const owner = await contract.owner()
-const functions = contract.interface.functions;
-const functionsIO = $ref({})
-for (const funcName of Object.keys(functions)) {
-  functionsIO[funcName] = {
-    inputs: [],
-    outputs: [],
-    error: null,
+
+async function execFunc (funcName) {
+  try {
+    const response = await contract[funcName](...functionsIO[funcName].inputs);
+
+    functionsIO[funcName].outputs[0] = response;
   }
-  for (const inputIndex in functions[funcName].inputs) {
-    functionsIO[funcName].inputs[inputIndex] = "";
-  }
-  for (const outputIndex in functions[funcName].outputs) {
-    functionsIO[funcName].outputs[outputIndex] = "";
+  catch (e) {
+    functionsIO[funcName].error = e.reason;
   }
 }
 
-
-const events = contract.interface.events;
-const eventsIO = $ref({})
+const functions = contract.interface.functions;
+const functionsIO = $ref({})
+for (const func of Object.values(functions)) {
+  functionsIO[func.name] = {
+    inputs: [],
+    outputs: [],
+    error: null,
+    properties: {
+      constant: func.constant,
+      payable: func.payable,
+      inputs: func.inputs,
+      outputs: func.outputs,
+    }
+  }
+  for (const inputIndex in functions[funcName].inputs) {
+    functionsIO[func.name].inputs[inputIndex] = "";
+  }
+  for (const outputIndex in functions[funcName].outputs) {
+    functionsIO[func.name].outputs[outputIndex] = "";
+  }
+}
 
 async function receiveEvent(event) {
   eventsIO[event.event].count += 1
@@ -43,23 +57,14 @@ async function receiveEvent(event) {
   eventsIO[event.event].logs.push(log)
 }
 
+const events = contract.interface.events;
+const eventsIO = $ref({})
 for (const event of Object.values(events)) {
   eventsIO[event.name] = {
     count: 0,
     logs: []
   }
   contract.on(event, receiveEvent)
-}
-
-async function execFunc (funcName) {
-  try {
-    const response = await contract[funcName](...functionsIO[funcName].inputs);
-
-    functionsIO[funcName].outputs[0] = response;
-  }
-  catch (e) {
-    functionsIO[funcName].error = e.reason;
-  }
 }
 
 function getType(type) {
@@ -79,11 +84,11 @@ function getType(type) {
     <li>Owner : {{ owner }}</li>
     <li>Functions :
       <ul>
-        <li v-for="(func, funcName) in functions">
-          <button @click="execFunc(funcName)">{{ func.name }} ({{ func.constant ? "read-only" : "" }}{{ func.payable ? ", payable" : "" }})</button>
-          <input v-for="(input, index) in func.inputs" v-model="functionsIO[funcName].inputs[index]" :type="getType(input.type)" :placeholder="input.type"/>
-          <input v-for="(output, index) in func.outputs" v-model="functionsIO[funcName].outputs[index]" type="text" :placeholder="output.type" disabled/>
-          <p v-if="functionsIO[funcName].error" style="color: red;">{{ functionsIO[funcName].error }}</p>
+        <li v-for="(func, funcName) in functionsIO">
+          <button @click="execFunc(funcName)">{{ funcName }} ({{ func.properties.constant ? "read-only" : "" }}{{ func.properties.payable ? ", payable" : "" }})</button>
+          <input v-for="(input, index) in func.properties.inputs" v-model="func.inputs[index]" :type="getType(input.type)" :placeholder="input.type"/>
+          <input v-for="(output, index) in func.properties.outputs" v-model="func.outputs[index]" type="text" :placeholder="output.type" disabled/>
+          <p v-if="func.error">{{ func.error }}</p>
         </li>
       </ul>
     </li>
@@ -92,10 +97,10 @@ function getType(type) {
       <ul>
         <li v-for="(event, eventName) in eventsIO">
           <h3>{{ eventName }}</h3>
-          <p>Count : {{ eventsIO[eventName].count }}</p>
+          <p>Count : {{ event.count }}</p>
           <p>Logs:</p>
           <ul>
-            <li v-for="log in eventsIO[eventName].logs">{{ log }}</li>
+            <li v-for="log in event.logs">{{ log }}</li>
           </ul>
         </li>
       </ul>
