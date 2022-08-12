f561d80080cefdea0456111d7238c152447c5980
Lila Rest <mail@lila.rest>
2022-08-12 18:54:16 +0200

    Move provider and signer initialization from the DApp store to index.js and them as raw to prevent immutable error

--- demo/frontend/src/main.js
+++ demo/frontend/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue"
 import { initVuethers } from "../../../src/index.js"
 import App from "./App.vue"
 import { vuethersConfig } from "../vuethers.config.js"
+import { createPinia } from "pinia";
 
 const app = createApp(App)
+app.use(createPinia, {})
 app.use(initVuethers, vuethersConfig)
 app.mount("#app")
