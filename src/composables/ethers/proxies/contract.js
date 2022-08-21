87c8bef8e8466982edcdf49926421070d536dc01
Lila Rest <mail@lila.rest>
2022-08-21 10:36:21 +0200

    - Move contracts-list.js under src/composables/ethers/ - Improve safers by removing nested 'on..' methods

--- src/composables/ethers/proxies/contract.js
+++ src/composables/ethers/proxies/contract.js
@@ -1,7 +1,7 @@
 import { EthersObjectProxy } from "./proxy.js";
 import { EthersContractExtension } from "../extensions/contract.js";
 import { dapp, Status } from "../../../index.js";
-import { computed, watch } from "vue";
+import { computed, watch, getCurrentInstance } from "vue";
 
 export class EthersContractProxy extends EthersObjectProxy {
   constructor (ethersObject) {
@@ -17,37 +17,34 @@ export class EthersContractProxy extends EthersObjectProxy {
   }
 
   onReadSafe (func) {
-    dapp.provider.onSafe((component) => {
-        if (this.isSafe.value) {
-            func(component)
-        }
-        else {
-            const unwatch = watch(this.isSafe, () => {
-                if (this.isSafe.value) {
-                    func(component)
-                    unwatch()
-                }
-            })
-        }
-    })
+    const component = getCurrentInstance();
+    if (this.isSafe.value) {
+        func(component)
+    }
+    else {
+        const unwatch = watch(this.isSafe, () => {
+            if (this.isSafe.value) {
+                func(component)
+                unwatch()
+            }
+        })
+    }
   }
 
   onWriteSafe (func) {
-    dapp.signer.onSafe((component) => {
-        if (this.isSafe.value) {
-            func(component)
-        }
-        else {
-            const unwatch = watch(this.isSafe, () => {
-                if (this.isSafe.value) {
-                    func(component)
-                    unwatch()
-                }
-            })
-        }
-    })
+    const component = getCurrentInstance();
+    if (this.isSafe.value) {
+        func(component)
+    }
+    else {
+        const unwatch = watch(this.isSafe, () => {
+            if (this.isSafe.value) {
+                func(component)
+                unwatch()
+            }
+        })
+    }
   }
-
 }
 
 
