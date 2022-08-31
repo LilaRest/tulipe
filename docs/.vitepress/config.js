17272e80754d7de377524e5f3d3583b57179e753
Lila Rest <mail@lila.rest>
2022-08-31 17:33:40 +0200

    Adjust logo responsiveness on documentation and add logo in navbar next to title

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -11,7 +11,7 @@ export default {
     repo: "https://github.com/LilaRest/vuethers",
     docsDir: "docs",
     docsBranch: "main",
-    logo: "",
+    logo: "https://static.vuethers.org/vuethers-logo.png",
     editLink: {
       pattern: "https://github.com/LilaRest/vuethers/edit/main/docs/:path",
       text: "Edit this page on GitHub"
