a07e494173d4e4df3fa37478544ee3824ad96f9a
0Lilian <cruaneslilian@gmail.com>
2022-09-01 13:52:18 +0200

    Fix release.config.js export error

--- release.config.js
+++ release.config.js
@@ -3,4 +3,4 @@ const releaseConfig = {
     branches: ['+([0-9])?(.{+([0-9]),x}).x', 'main', 'next', 'next-major', {name: 'beta', prerelease: true}, {name: 'alpha', prerelease: true}]
   }
 }
-module.exports = { releaseConfig }
+module.exports = releaseConfig
