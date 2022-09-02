e35f52b75abc5a29bc3331d62753fa799f666b4d
CI/CD <cicd@tuli.pe>
2022-09-02 21:59:36 +0000

    CI/CD : New dist files built under  directory

---
+++ dist/index.25897989.mjs
@@ -0,0 +1,24 @@
+import { ConnectWalletButton as a, ContractInteractor as o, DebugBar as c, EventInteractor as S, EventsInteractor as p, InputUnits as f, MethodInteractor as l, MethodsInteractor as d, OnContractReadSafe as i, OnContractWriteSafe as s, OnContractsReadSafe as O, OnContractsWriteSafe as C, OnDappSafe as I, OnProviderSafe as v, OnSignerSafe as D, SelectNetworkDropdown as W, SelectWalletDropdown as m, Transact as u, styleLevelProp as w } from "./tulipe.es.js";
+import "vue";
+import "ethers";
+export {
+  a as ConnectWalletButton,
+  o as ContractInteractor,
+  c as DebugBar,
+  S as EventInteractor,
+  p as EventsInteractor,
+  f as InputUnits,
+  l as MethodInteractor,
+  d as MethodsInteractor,
+  i as OnContractReadSafe,
+  s as OnContractWriteSafe,
+  O as OnContractsReadSafe,
+  C as OnContractsWriteSafe,
+  I as OnDappSafe,
+  v as OnProviderSafe,
+  D as OnSignerSafe,
+  W as SelectNetworkDropdown,
+  m as SelectWalletDropdown,
+  u as Transact,
+  w as styleLevelProp
+};
