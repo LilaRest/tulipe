b4eadccd4e14f6189a7a201d31d2a3ec5fff7c5e
Lila Rest <mail@lila.rest>
2022-09-04 17:39:46 +0200

    chore: remove scripts which are now useless

--- scripts/minor.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull --no-edit &&
-npm version --no-git-tag-version minor &&
-git add -A &&
-git commit &&
-git push &&
-echo "New minor version successfully pushed."
--- scripts/patch.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git pull --no-edit &&
-npm version --no-git-tag-version patch &&
-git add -A &&
-git commit &&
-git push &&
-echo "New patch version successfully pushed."
