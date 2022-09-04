1b6cdaf00129f1b967eb0322906519ad4dde0922
Lila Rest <mail@lila.rest>
2022-09-04 17:16:26 +0200

    feat: support wallets that doesn't allow lazy connect

--- src/composables/wallets/errors.js
+++ src/composables/wallets/errors.js
@@ -1,7 +1,13 @@
 export class WalletConnectionRejected extends Error {
-  constructor(walletName, ...args) {
-    super(walletName, ...args);
-    this.message = `${walletName} wallet has rejected the connection request.`;
+  constructor(walletId, ...args) {
+    super(walletId, ...args);
+    this.message = `'${walletId}' wallet has rejected the connection request.`;
   }
 }
 
+export class WalletMethodNotImplemented extends Error {
+  constructor(message, ...args) {
+    super(walletId, ...args);
+    this.message = message
+  }
+}
--- src/composables/wallets/wallet.js
+++ src/composables/wallets/wallet.js
@@ -1,10 +1,33 @@
+import { WalletMethodNotImplemented } from "./errors.js";
+
 export class Wallet {
 
     constructor (id) {
-      this.id = id;
+      this.id = id; // The Tulipe's wallet ID of the wallet
+      this.lazyConnectAvailable = true; // Set to true if lazy connection is available for that wallet
     }
 
-    async connect () {
-      throw `connect() method of ${this.id} wallet is not implemented.`
+    isConnected () {
+      throw new WalletMethodNotImplemented(`isConnected() method is not implemented in ${this.id} wallet class.`)
+    }
+
+    getProvider () {
+      throw new WalletMethodNotImplemented(`getProvider() method is not implemented in ${this.id} wallet class.`)
+    }
+
+    getSigner (lazy=false) {
+      throw new WalletMethodNotImplemented(`getSigner() method is not implemented in ${this.id} wallet class.`)
+    }
+
+    async connect (lazy=false) {
+      throw new WalletMethodNotImplemented(`connect() method is not implemented in ${this.id} wallet class.`)
+    }
+
+    addNetwork (id) {
+      throw new WalletMethodNotImplemented(`addNetwork() method is not implemented in ${this.id} wallet class.`)
+    }
+
+    switchNetwork (id, lazy=false) {
+      throw new WalletMethodNotImplemented(`switchNetwork() method is not implemented in ${this.id} wallet class.`)
     }
 }
