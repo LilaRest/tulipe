7b0803ba5a2c5450fea9f12f54f0ceb14d80f7ef
Lila Rest <mail@lila.rest>
2022-09-04 21:24:06 +0200

    feat: add Tulipe wrapper for all Ethers.js provider classes

--- src/composables/index.js
+++ src/composables/index.js
@@ -2,5 +2,6 @@ export { Status } from "./status.js";
 export * from "./dapp/index.js";
 export * from "./config/index.js";
 export * from "./proxies/index.js";
+export * from "./ethers/index.js";
 export * from "./chain-watchers/index.js";
 export * from "./wallets/index.js";
